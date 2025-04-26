94a35d3c7529fe834f313635f1dc55894d660dff
RehanAli21 <rehan.shah70@yahoo.com>
2025-04-26 12:06:19 +0500

    page dragging functionality added.

--- js_scripts/drag_mode.js
+++ js_scripts/drag_mode.js
@@ -1,35 +1,56 @@
-const box = document.getElementById('page')
+const box = document.getElementById('pages_container')
 const box_container = document.getElementById('page_viewer')
 
-let isDragging = false
-let offsetX, offsetY
+let move = false
+let oldX = 0
+let oldY = 0
 
-// Store original position
-const originalPosition = {
-	left: box.offsetLeft,
-	top: box.offsetTop,
+let x_translate = 0
+let y_translate = 0
+
+function changeState() {
+	box.style.transform = `translate(${x_translate}px, ${y_translate}px)`
 }
 
-box.addEventListener('mousedown', e => {
-	isDragging = true
-	offsetX = e.clientX - box.offsetLeft
-	offsetY = e.clientY - box.offsetTop
-	box.classList.add('dragging')
+box_container.addEventListener('mousedown', () => {
+	move = true
+	box_container.style.cursor = 'grabbing'
 })
 
-document.addEventListener('mousemove', e => {
-	if (isDragging) {
-		box.style.left = e.clientX - offsetX + 'px'
-		box.style.top = e.clientY - offsetY + 'px'
+function disableMovement() {
+	move = false
+	box_container.style.cursor = ''
+}
+
+box_container.addEventListener('mouseup', disableMovement)
+box_container.addEventListener('mouseleave', disableMovement)
+
+function resetEveryThing() {
+	box.style.transform = ''
+	oldX = 0
+	oldY = 0
+	x_translate = 0
+	y_translate = 0
+}
+box_container.addEventListener('dblclick', resetEveryThing)
+
+box_container.addEventListener('mousemove', e => {
+	if (move) {
+		if (oldX < e.pageX) {
+			x_translate += 10
+		} else if (oldX > e.pageX) {
+			x_translate -= 10
+		}
+
+		if (oldY < e.pageY) {
+			y_translate += 10
+		} else if (oldY > e.pageY) {
+			y_translate -= 10
+		}
+
+		oldX = e.pageX
+		oldY = e.pageY
+
+		changeState()
 	}
-})
-
-document.addEventListener('mouseup', () => {
-	isDragging = false
-	box.classList.remove('dragging')
-})
-
-box_container.addEventListener('dblclick', () => {
-	box.style.left = originalPosition.left + 'px'
-	box.style.top = originalPosition.top + 'px'
 })
