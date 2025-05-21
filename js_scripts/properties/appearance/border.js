44e1b714583971449fe63d7cf16941c65dd5cfff
RehanAli21 <rehan.shah70@yahoo.com>
2025-05-21 23:07:30 +0500

    border sides button ui is working but funcionality is not.

--- js_scripts/properties/appearance/border.js
+++ js_scripts/properties/appearance/border.js
@@ -1,24 +1,35 @@
 import { changeElementStyle, saveDataIntoElement } from '../property_base.js'
 
-export default function setUpBorder() {
-	const borderToggler = document.getElementById('appeareance_border_toggle')
-	const borderColorInput = document.getElementById('appeareance_border_color_input')
-	const borderSizeInput = document.getElementById('appeareance_border_size_input')
-	const borderRadiusInput = document.getElementById('appeareance_border_radius_input')
-	const borderStyleSelect = document.getElementById('appeareance_border_style_select')
+let borderProperties = ['border-left', 'border-right', 'border-top', 'border-bottom']
 
+const borderToggler = document.getElementById('appeareance_border_toggle')
+const borderColorInput = document.getElementById('appeareance_border_color_input')
+const borderSizeInput = document.getElementById('appeareance_border_size_input')
+const borderRadiusInput = document.getElementById('appeareance_border_radius_input')
+const borderStyleSelect = document.getElementById('appeareance_border_style_select')
+const borderLeftBtn = document.getElementById('appeareance_border_side_L')
+const borderRightBtn = document.getElementById('appeareance_border_side_R')
+const borderTopBtn = document.getElementById('appeareance_border_side_T')
+const borderBottomBtn = document.getElementById('appeareance_border_side_B')
+
+export default function setUpBorder() {
 	if (borderToggler) {
 		borderToggler.addEventListener('focusout', saveDataIntoElement)
 
 		toggleBorderStyles(borderToggler)
 
-		borderToggler.addEventListener('change', () => toggleBorderStyles(borderToggler))
+		borderToggler.addEventListener('change', () => {
+			borderSizeBtns()
+			toggleBorderStyles(borderToggler)
+		})
 	}
 
 	if (borderColorInput) {
 		borderColorInput.addEventListener('focusout', saveDataIntoElement)
 
-		borderColorInput.addEventListener('input', () => changeElementStyle(['border-color'], borderColorInput, 'no_value'))
+		borderColorInput.addEventListener('input', () =>
+			changeElementStyle(getBorderPropertyWithSpecificValue('color'), borderColorInput, 'no_value')
+		)
 	}
 
 	if (borderSizeInput) {
@@ -40,6 +51,22 @@ export default function setUpBorder() {
 
 		borderStyleSelect.addEventListener('change', () => changeElementStyle(['border-style'], borderStyleSelect, 'no_value'))
 	}
+
+	if (borderBottomBtn) {
+		borderBottomBtn.addEventListener('click', () => toggleBordeeSide('border-bottom'))
+	}
+
+	if (borderTopBtn) {
+		borderTopBtn.addEventListener('click', () => toggleBordeeSide('border-top'))
+	}
+
+	if (borderLeftBtn) {
+		borderLeftBtn.addEventListener('click', () => toggleBordeeSide('border-left'))
+	}
+
+	if (borderRightBtn) {
+		borderRightBtn.addEventListener('click', () => toggleBordeeSide('border-right'))
+	}
 }
 
 function toggleBorderStyles(borderToggler) {
@@ -51,5 +78,45 @@ function toggleBorderStyles(borderToggler) {
 
 	let borderValue = borderToggler.checked ? '1px solid black' : ''
 
-	changeElementStyle(['border'], { value: borderValue }, 'no_value')
+	changeElementStyle(borderProperties, { value: borderValue }, 'no_value')
+}
+
+function getBorderPropertyWithSpecificValue(value) {
+	return borderProperties.map(e => `${e}-${value}`)
+}
+
+function toggleBordeeSide(value) {
+	if (borderProperties.includes(value)) {
+		borderProperties = borderProperties.filter(e => e !== value)
+	} else {
+		borderProperties.push(value)
+	}
+
+	borderSizeBtns()
+}
+
+function borderSizeBtns() {
+	if (borderProperties.includes('border-left')) {
+		borderLeftBtn.style.backgroundColor = 'var(--primary)'
+	} else {
+		borderLeftBtn.style.backgroundColor = ''
+	}
+
+	if (borderProperties.includes('border-right')) {
+		borderRightBtn.style.backgroundColor = 'var(--primary)'
+	} else {
+		borderRightBtn.style.backgroundColor = ''
+	}
+
+	if (borderProperties.includes('border-top')) {
+		borderTopBtn.style.backgroundColor = 'var(--primary)'
+	} else {
+		borderTopBtn.style.backgroundColor = ''
+	}
+
+	if (borderProperties.includes('border-bottom')) {
+		borderBottomBtn.style.backgroundColor = 'var(--primary)'
+	} else {
+		borderBottomBtn.style.backgroundColor = ''
+	}
 }
