f9c90a9ee3ca79f2cac3b1e56837b278f8b68b13
RehanAli21 <rehan.shah70@yahoo.com>
2025-05-25 12:55:40 +0500

    show and hide added for appereance and text.

--- js_scripts/show_hide_properties.js
+++ js_scripts/show_hide_properties.js
@@ -1,26 +1,45 @@
 const transformSectionHead = document.getElementById('transform_section_head')
-let showTransformSectionProperties = true
+const appearanceSectionHead = document.getElementById('appeareance_section_head')
+const textSectionHead = document.getElementById('text_section_head')
 
 const showProperties = {
 	transform: true,
+	appearance: true,
+	text: true,
 }
 
 if (transformSectionHead) {
-	toggleProperties(false, 'transform', 'transform_section', 'property_section', transformSectionHead)
+	toggleProperties(false, 'transform', 'transform_section', ['property_section'], transformSectionHead)
 	transformSectionHead.addEventListener('click', () =>
-		toggleProperties(true, 'transform', 'transform_section', 'property_section', transformSectionHead)
+		toggleProperties(true, 'transform', 'transform_section', ['property_section'], transformSectionHead)
 	)
 }
 
-function toggleProperties(change, property, parentId, classOfElementToTogglevisibility, ele) {
+if (appearanceSectionHead) {
+	toggleProperties(false, 'transform', 'appeareance_section', ['property_section', 'property_section_no_class'], appearanceSectionHead)
+	appearanceSectionHead.addEventListener('click', () =>
+		toggleProperties(true, 'transform', 'appeareance_section', ['property_section', 'property_section_no_class'], appearanceSectionHead)
+	)
+}
+
+if (textSectionHead) {
+	toggleProperties(false, 'transform', 'text_section', ['property_section', 'property_section_no_class'], textSectionHead)
+	textSectionHead.addEventListener('click', () =>
+		toggleProperties(true, 'transform', 'text_section', ['property_section', 'property_section_no_class'], textSectionHead)
+	)
+}
+
+function toggleProperties(change, property, parentId, classesOfElementToTogglevisibility, ele) {
 	if (change) showProperties[property] = !showProperties[property]
 
 	const parent = document.getElementById(parentId)
 
 	if (parent) {
 		for (let child of parent.children) {
-			if (child.classList.contains(classOfElementToTogglevisibility)) {
-				child.style.display = showProperties[property] ? '' : 'none'
+			for (let classOfElementToTogglevisibility of classesOfElementToTogglevisibility) {
+				if (child.classList.contains(classOfElementToTogglevisibility)) {
+					child.style.display = showProperties[property] ? '' : 'none'
+				}
 			}
 		}
 
