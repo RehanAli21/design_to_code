2e73ad10ae1372f6a4e5db24de56dd2a75b13f1b
RehanAli21 <rehan.shah70@yahoo.com>
2025-05-31 11:46:01 +0500

    background color change on clicking text align btn

--- js_scripts/properties/textAlign/align.js
+++ js_scripts/properties/textAlign/align.js
@@ -9,20 +9,50 @@ export default function setUpTextAlignment() {
 	textAlignLeftBtn.addEventListener('click', () => {
 		changeElementStyle(['text-align'], { value: 'left' }, 'no_value')
 		saveDataIntoElement()
+		setUpBtnBackground('left')
 	})
 
 	textAlignRightBtn.addEventListener('click', () => {
 		changeElementStyle(['text-align'], { value: 'right' }, 'no_value')
 		saveDataIntoElement()
+		setUpBtnBackground('right')
 	})
 
 	textAlignCenterBtn.addEventListener('click', () => {
 		changeElementStyle(['text-align'], { value: 'center' }, 'no_value')
 		saveDataIntoElement()
+		setUpBtnBackground('center')
 	})
 
 	textAlignJustifyBtn.addEventListener('click', () => {
 		changeElementStyle(['text-align'], { value: 'justify' }, 'no_value')
 		saveDataIntoElement()
+		setUpBtnBackground('justify')
 	})
+}
+
+function setUpBtnBackground(currentAlignValue) {
+	if (currentAlignValue == 'left') {
+		textAlignLeftBtn.style.backgroundColor = 'var(--primary)'
+	} else {
+		textAlignLeftBtn.style.backgroundColor = ''
+	}
+
+	if (currentAlignValue == 'right') {
+		textAlignRightBtn.style.backgroundColor = 'var(--primary)'
+	} else {
+		textAlignRightBtn.style.backgroundColor = ''
+	}
+
+	if (currentAlignValue == 'center') {
+		textAlignCenterBtn.style.backgroundColor = 'var(--primary)'
+	} else {
+		textAlignCenterBtn.style.backgroundColor = ''
+	}
+
+	if (currentAlignValue == 'justify') {
+		textAlignJustifyBtn.style.backgroundColor = 'var(--primary)'
+	} else {
+		textAlignJustifyBtn.style.backgroundColor = ''
+	}
 }
