4c659910aa388868ddcbb16c06c6b40746f10b09
RehanAli21 <rehan.shah70@yahoo.com>
2025-06-02 12:14:22 +0500

    slef align btn background color change on click.

--- js_scripts/properties/selfAlign/align.js
+++ js_scripts/properties/selfAlign/align.js
@@ -9,17 +9,40 @@ export default function setUpSelfAlignment() {
 		changeElementStyle(['margin-left'], { value: '0px' }, 'no_value')
 		changeElementStyle(['margin-right'], { value: 'auto' }, 'no_value')
 		saveDataIntoElement()
+		setUpSelfAlignBtnBackground('left')
 	})
 
 	selfAlignCenterBtn.addEventListener('click', () => {
 		changeElementStyle(['margin-left'], { value: 'auto' }, 'no_value')
 		changeElementStyle(['margin-right'], { value: 'auto' }, 'no_value')
 		saveDataIntoElement()
+		setUpSelfAlignBtnBackground('center')
 	})
 
 	selfAlignRightBtn.addEventListener('click', () => {
 		changeElementStyle(['margin-left'], { value: 'auto' }, 'no_value')
 		changeElementStyle(['margin-right'], { value: '0px' }, 'no_value')
 		saveDataIntoElement()
+		setUpSelfAlignBtnBackground('right')
 	})
+}
+
+export function setUpSelfAlignBtnBackground(currentSelfAlignValue) {
+	if (currentSelfAlignValue == 'left') {
+		selfAlignLeftBtn.style.backgroundColor = 'var(--primary)'
+	} else {
+		selfAlignLeftBtn.style.backgroundColor = ''
+	}
+
+	if (currentSelfAlignValue == 'center') {
+		selfAlignCenterBtn.style.backgroundColor = 'var(--primary)'
+	} else {
+		selfAlignCenterBtn.style.backgroundColor = ''
+	}
+
+	if (currentSelfAlignValue == 'right') {
+		selfAlignRightBtn.style.backgroundColor = 'var(--primary)'
+	} else {
+		selfAlignRightBtn.style.backgroundColor = ''
+	}
 }
