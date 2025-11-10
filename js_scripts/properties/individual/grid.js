e825c3522e7a92cc7c4be3741acebefc504769c7
RehanOnText <rehan.shah@talkontext.com>
2025-11-11 00:22:55 +0500

    grid UI working.

--- js_scripts/properties/individual/grid.js
+++ js_scripts/properties/individual/grid.js
@@ -3,6 +3,12 @@ import { changeElementStyle, removeElementStyle, saveDataIntoElement } from '../
 
 const gridToggler = document.getElementById('individual_grid_toggle')
 
+const gridColToggler = document.getElementById('grid_col_btn')
+const gridColNumInput = document.getElementById('cols-number-input')
+
+const gridRowToggler = document.getElementById('grid_row_btn')
+const gridRowNumInput = document.getElementById('rows-number-input')
+
 export default function setUpGrid() {
 	if (gridToggler) {
 		gridToggler.addEventListener('focusout', saveDataIntoElement)
@@ -13,11 +19,29 @@ export default function setUpGrid() {
 			toggleGridStyles(gridToggler, gridToggler.checked)
 		})
 	}
+
+	gridColToggler.addEventListener('click', () => {
+		removeRows()
+		changeElementStyle(['grid-template-columns'], { value: '1fr' }, 'no_value')
+		saveDataIntoElement()
+
+		gridColNumInput.style.display = 'grid'
+		gridColToggler.style.backgroundColor = 'var(--primary)'
+	})
+
+	gridRowToggler.addEventListener('click', () => {
+		removeCols()
+		changeElementStyle(['grid-template-rows'], { value: '1fr' }, 'no_value')
+		saveDataIntoElement()
+
+		gridRowNumInput.style.display = 'grid'
+		gridRowToggler.style.backgroundColor = 'var(--primary)'
+	})
 }
 
 export function toggleGridStyles(gridToggler, show) {
 	for (const child of gridToggler.parentElement.parentElement.children) {
-		if (child.classList.contains('property_section')) {
+		if (child.classList.contains('property_section_head')) {
 			child.style.display = show ? '' : 'none'
 		}
 	}
@@ -33,4 +57,16 @@ function removeAllGridProperties() {
 	let gridProperties = ['display', 'grid-template-columnss', 'grid-template-rows']
 	removeElementStyle(gridProperties)
 	saveDataIntoElement()
+}
+
+function removeRows() {
+	removeElementStyle(['grid-template-rows'])
+	gridRowNumInput.style.display = 'none'
+	gridRowToggler.style.backgroundColor = ''
+}
+
+function removeCols() {
+	removeElementStyle(['grid-template-columns'])
+	gridColNumInput.style.display = 'none'
+	gridColToggler.style.backgroundColor = ''
 }
--- js_scripts/updatePropertyInUI/individual/update_individual_ui.js
+++ js_scripts/updatePropertyInUI/individual/update_individual_ui.js
@@ -1,5 +1,7 @@
 import { ElementData } from '../../data/element_data.js'
 import { InputTypes } from '../../data/enums.js'
+import { getStyles } from '../updatePropertyUIUtils.js'
+import { toggleGridStyles } from '../../properties/individual/grid.js'
 
 export default function updateIndividualPropertyUI() {
 	const innerTextArea = document.getElementById('text_property_textarea')
@@ -23,5 +25,46 @@ export default function updateIndividualPropertyUI() {
 		if (ElementData.inputType == InputTypes[key]) {
 			inputTypeSelect.value = key
 		}
+	}
+
+	const gridCheckBox = document.getElementById('individual_grid_toggle')
+
+	const gridColToggler = document.getElementById('grid_col_btn')
+	const gridColNumInput = document.getElementById('cols-number-input')
+
+	const gridRowToggler = document.getElementById('grid_row_btn')
+	const gridRowNumInput = document.getElementById('rows-number-input')
+
+	const styles = getStyles()
+
+	console.log(styles, 'styles')
+
+	if (Object.hasOwn(styles, 'display') && styles.display === 'grid') {
+		gridCheckBox.checked = true
+		toggleGridStyles(gridCheckBox, true)
+
+		if (Object.hasOwn(styles, 'grid-template-columns')) {
+			gridColToggler.style.backgroundColor = 'var(--primary)'
+			gridColNumInput.style.display = 'grid'
+		} else {
+			gridColToggler.style.backgroundColor = ''
+			gridColNumInput.style.display = 'none'
+		}
+
+		if (Object.hasOwn(styles, 'grid-template-rows')) {
+			gridRowToggler.style.backgroundColor = 'var(--primary)'
+			gridRowNumInput.style.display = 'grid'
+		} else {
+			gridRowToggler.style.backgroundColor = ''
+			gridRowNumInput.style.display = 'none'
+		}
+	} else {
+		gridCheckBox.checked = false
+
+		toggleGridStyles(gridCheckBox, false)
+
+		gridColNumInput.style.display = 'none'
+
+		gridRowNumInput.style.display = 'none'
 	}
 }
